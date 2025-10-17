396004c5e0a6be4a9a5f015722d8eaf4e342d31b
NickolaiFredheim <nicfre00@gmail.com>
2025-10-17 16:09:07 +0200

    har laget notebook og gjort en liten endring i analysene

--- Analyse.py
+++ Analyse.py
@@ -1,4 +1,8 @@
 #dette er en testfil
+#nå skriver jeg kode her..! 
 
+import pandas as pd
 
-nå skriver jeg kode her..! 
+Vannforing = pd.read_csv('62.5.0-Vannføring-dogn-v1.csv', delim_whitespace=True, header=None)
+
+print(Vannforing.head())
