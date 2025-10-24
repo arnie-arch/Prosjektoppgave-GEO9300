2185b7753d5756e1a312ca7b4bf8b525b30a43d3
Anne <spamtilanne@gmail.com>
2025-10-24 15:13:29 +0200

    Add settings file and update data analysis script
    - Created a new settings.json file to configure Python REPL settings.
    - Updated Analyse.py to improve data loading and visualization, including renaming columns for clarity.

--- Analyse.py
+++ Analyse.py
@@ -1,8 +1,17 @@
-#dette er en testfil
-#nå skriver jeg kode her..! 
-
+#Libraries
 import pandas as pd
+import matplotlib.pyplot as plt
 
-Vannforing = pd.read_csv('62.5.0-Vannføring-dogn-v1.csv', delim_whitespace=True, header=None)
+vf = pd.read_csv('62.5.0-Vannføring-dogn-v1.csv', delimiter=';', on_bad_lines='skip', skiprows=[0])
 
-print(Vannforing.head())
+print(vf.head())
+
+#vf1 = Data fra 1991 - 2020
+#vf2 = Data fra 1961 - 1990
+
+vf1 = vf[(vf['Tidspunkt'] >= '1991-01-01') & (vf['Tidspunkt'] <= '2020-12-31')]
+print(vf1.head())
+vf2 = vf[(vf['Tidspunkt'] >= '1961-01-01') & (vf['Tidspunkt'] <= '1990-12-31')]
+print(vf2.tail())
+
+plt.plot(vf1['Tidspunkt'], vf1['Vannføring'], label='1991-2020')
